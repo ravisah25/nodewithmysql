8e81fcad9628015873f6892f6311b083fad6be10
unknown <rshankar.raja@gmail.com>
2019-01-24 22:19:43 +0530

    acccess control

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const bodyParser = require('body-parser');
 const baseRouter = require('./src/router/index')
 
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 mySqlConnect.connect((err) => {
     if (err) {
